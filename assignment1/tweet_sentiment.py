ea162550489a4b9dd00e7d3f858ccbef5037a00f
alprsln <alpars44@hotmail.com>
2020-10-02 10:37:37 +0300

    Clear spaces in output and solve problem 2

--- assignment1/tweet_sentiment.py
+++ assignment1/tweet_sentiment.py
@@ -1,7 +1,6 @@
 import sys
-
-def hw():
-    print 'Hello, world!'
+import json
+import re
 
 def lines(fp):
     print str(len(fp.readlines()))
@@ -9,9 +8,25 @@ def lines(fp):
 def main():
     sent_file = open(sys.argv[1])
     tweet_file = open(sys.argv[2])
-    hw()
-    lines(sent_file)
-    lines(tweet_file)
+
+    afinnfile = sent_file #open("AFINN-111.txt")
+    scores = {}
+    for line in afinnfile:
+        term, score  = line.split("\t")  # The file is tab-delimited. "\t" means "tab character"
+        scores[term] = int(score)  
+    #print scores.items()
+    
+    for line in tweet_file:
+        sum = 0
+        tweetText = json.loads(line)["data"]["text"].encode('utf-8') #parse every tweet text
+        #print re.findall(r"[\w']+", tweetText)
+        for word in re.findall(r"[\w']+", tweetText): #Regex each word out
+            if scores.has_key(word.lower()):
+                sum += scores[word.lower()]
+        print sum
+
+    #lines(sent_file)
+    #lines(tweet_file)
 
 if __name__ == '__main__':
     main()
